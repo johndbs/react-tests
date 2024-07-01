12da84cb1e2ec650cca5a02b500e7cdb81f9067b
johndbs <83272366+johndbs@users.noreply.github.com>
2024-07-01 16:31:30 +0200

    feat: Add Calculator component to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import s from "./App.module.css";
+import { Calculator } from "./components/Calculator";
 export function App() {
-  return <div className={s.root}>Let's test :)</div>;
+  return <div className={s.root}>Let's test :)
+    <Calculator defaultA={3} defaultB={2} defaultOperator={"+"} />
+  </div>;
 }
---
+++ src/components/Calculator.js
@@ -0,0 +1,66 @@
+import { useState } from "react"
+import { divide, multiply, substract, sum } from "../utils/math-functions";
+
+const OPERATORS = ['+', '-', 'x', '/'];
+
+export function Calculator({defaultA, defaultB, defaultOperator}){
+
+    const [inputValueA, setInputValueA] = useState(!defaultA || isNaN(defaultA) ? 0 : Number(defaultA));
+    const [inputValueB, setInputValueB] = useState(!defaultB || isNaN(defaultB) ? 0 : Number(defaultB)) ;
+    const [operator, setOperator] = useState(OPERATORS.includes(defaultOperator) ? defaultOperator : '+');
+
+
+    const getResult = () => {
+        switch(operator) {
+            case '+':
+                return sum(inputValueA, inputValueB);
+            case '-':
+                return substract(inputValueA,inputValueB);
+            case 'x':
+                return multiply(inputValueA, inputValueB);
+            case '/':
+                return divideSafely(inputValueA, inputValueB)
+            default: 
+                return "No operator provided";
+        }
+    };
+
+    const divideSafely = (a, b) => {
+        try{
+            divide(a,b);
+        }catch(err){
+            return err.message;
+        }
+    }
+
+    const renderInputA = () =>{
+        return (<input value={inputValueA} type="number" 
+            onChange={(e) => setInputValueA(e.target.value ? Number.parseFloat(e.target.value) : 0)} />) ;
+    };
+
+    const renderInputB = () =>{
+        return (<input value={inputValueB} type="number" 
+            onChange={(e) => setInputValueB(e.target.value ? Number.parseFloat(e.target.value) : 0)} />) ;
+    };
+
+    const renderSelectOperator = () => {
+        return(<select value={operator} onChange={(e) => setOperator(e.target.value)}>
+            <option value="+">+</option>
+            <option value="-">-</option>
+            <option value="x">x</option>
+            <option value="/">/</option>
+        </select>);
+    };
+
+
+    return <>
+        <h1 style={{marginBottom: 40}}>Calculator</h1>
+
+        {renderInputA()}
+        {renderInputB()}
+        {renderSelectOperator()}
+
+        <h2 style={{margin: 20}}>Result</h2>
+        {getResult()}
+    </>
+}
