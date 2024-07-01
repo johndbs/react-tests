9af4bd3528dd900980bb52c83d7b1b7c59bc8967
johndbs <83272366+johndbs@users.noreply.github.com>
2024-07-01 15:41:38 +0200

    Prepare the base of the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import s from "./App.module.css";
+export function App() {
+  return <div className={s.root}>Let's test :)</div>;
 }
-
-export default App;
